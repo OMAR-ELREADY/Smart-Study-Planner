e31029db89b89b3a05105a9dd1ebe66a1c890f55
OMAR ELREEDY <omar.m3afn@gmail.com>
2026-04-09 21:16:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Indexers;
+
+namespace Indexers
+{ 
+    class ValuePrintWidth
+    {
+        public int DefaultCharWidth { get; private set; }
+        public ValuePrintWidth(int defaultCharacterWidth)
+        {
+            DefaultCharWidth = defaultCharacterWidth;
+        }
+
+        // First indexer
+        public int this[string s, int characterWidth]
+        {
+            get { return s.Length * characterWidth; }
+        }
+
+        // Second indexer
+        public int this[double x, int characterWidth]
+        {
+            get                                           
+            {
+                string s = Convert.ToString(x);
+                return this[s, characterWidth];
+            }
+        }
+
+
+        // Third indexer
+        public int this[string s]
+        {
+            get
+            {
+                return this[s, DefaultCharWidth];
+            }
+        }
+
+        // Fourth indexer
+        public int this[double x]
+        {
+            get
+            {
+                string s = Convert.ToString(x);
+                return this[s];
+            }
+        }
+    }
+                                                                                                              16.649
+    class Program
+    {
+        static void Main()
+        {
+            ValuePrintWidth x = new ValuePrintWidth(10);
+
+            Console.WriteLine(x["Ziad"]);
+            Console.WriteLine(x[16.649]);
+            Console.WriteLine(x["Ziad", 20]);
+            Console.WriteLine(x[16.649, 20]);
+        }
+    }
+}
